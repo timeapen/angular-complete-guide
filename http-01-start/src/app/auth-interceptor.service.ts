f73982fa2846e9286e0609f0747ada3425e53eb3
tim.eapen <tim.eapen@gmail.com>
2024-03-22 13:14:08 -0400

    respone interceptor - intercepting response

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
 
@@ -8,7 +9,15 @@ export class AuthInterceptorService implements HttpInterceptor {
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
         });
-        return next.handle(modifiedRequest);
+        return next.handle(modifiedRequest)
+            .pipe(
+                tap(event => {
+                    console.debug('Received event: ', event);
+                    if (event.type === HttpEventType.Response) {
+                        console.debug('Got response: ', event.body);
+                    }
+                })
+            )
     }
 
 }
