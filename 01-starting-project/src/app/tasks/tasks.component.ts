9afaed528a4049d70995d0ff44441ea79fde33db
tim.eapen <tim.eapen@gmail.com>
2025-05-12 17:03:20 -0400

    45. outputing user tasks

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -10,8 +10,38 @@ import { TaskComponent } from "./task/task.component";
 })
 export class TasksComponent {
 
-  @Input() name?: string;
-    // @Input({required: true}) name!: string;
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
 
+  // @Input() name?: string;
+    @Input({required: true}) name!: string;
+    @Input({required: true}) userId!: string;
+
+    get userTasks() {
+      return this.tasks.filter((task) => task.userId === this.userId);
+    }
 
 }
