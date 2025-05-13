34d1a19812449755bd68ceb7b8fa0df290cda886
tim.eapen <tim.eapen@gmail.com>
2025-05-12 20:12:43 -0400

    47. storing data models in separate files

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, Output, computed, input, output } from '@angular/core';
+import { type User } from './user.model';
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
 
 @Component({
   selector: 'app-user',
---
+++ 01-starting-project/src/app/user/user.model.ts
@@ -0,0 +1,12 @@
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+export interface User {
+    id: string;
+    avatar: string;
+    name: string;
+  }
+  
