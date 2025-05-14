e09c14074186beab5c2cb5c7054d8ba00270cff1
tim.eapen <tim.eapen@gmail.com>
2025-05-13 22:15:44 -0400

    52. 2 way data binding

--- 01-starting-project/src/app/tasks/add-task/add-task.component.ts
+++ 01-starting-project/src/app/tasks/add-task/add-task.component.ts
@@ -1,15 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
 
   @Output() cancelAddTask = new EventEmitter<void>();
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDueDate: string = '';
+
 
   onCancel() {
     console.debug('Cancel add task');
