36a13754f4fef82bcb6473a28b8a60c4d9cf041b
tim.eapen <tim.eapen@gmail.com>
2025-05-14 16:51:59 -0400

    56. conntent projection with ng- content

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output, computed, input, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
